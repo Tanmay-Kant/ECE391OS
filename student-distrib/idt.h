fa23047f2a1f41dd4bcfdcc71cf17335793c901f
TanmayKant <tanmaykant2.0@gmail.com>
2023-03-21 05:37:40 -0500

    Made it to double boot and then sitting at desired screen, will seek TA assistance

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -2,7 +2,6 @@
 #define _IDT_H
 
 #include "x86_desc.h"
-#include "exception_linkage.h"
 #include "lib.h"
 #define NUM_EXCEPTIONS      21
 #define SYSCALL_VEC_NUM     (0x80)
@@ -16,6 +15,27 @@ void idt_init(); //Initializes the IDT and sets IDT entries
 
 void exception_handler(uint32_t vector); //printing support for the corresponding exception
 
+void division_error(void);
+void debug(void);
+void non_maskable_interrupt(void);
+void breakpoint(void);
+void overflow(void);
+void bound_range_exceeded(void);
+void invalid_opcode(void);
+void device_not_available(void);
+void double_fault(void);
+void coprocessor_segment_overrun(void);
+void invalid_tss(void);
+void segment_not_present(void);
+void stack_segment_fault(void);
+void general_protection_fault(void);
+void page_fault(void);
+void floating_point_exception(void);
+void alignment_check(void);
+void machine_check(void);
+void simd_floating_point_exception(void);
+
+void system_calls(void);
 
 
 #endif /* _IDT_H */
